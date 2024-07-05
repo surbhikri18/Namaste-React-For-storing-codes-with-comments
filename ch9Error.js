29f00d0c85582a36d61ca9ab8fba746fa217343a
surbhikri18 <sk3519715@gmail.com>
2024-07-06 03:54:17 +0530

    Create ch9Error.js

---
+++ ch9Error.js
@@ -0,0 +1,18 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+    const err = useRouteError();
+    console.log(err);
+    //destructuring
+    // const {status, statusText} = err;
+    return(
+        <div>
+            <h1>Oops!!</h1>
+            <h2>Something went Wrong!!</h2>
+            <h2>{err.status +":" +err.statusText}</h2>
+            {/* <h2>{status +":" +statusText}</h2> */}
+        </div>
+    )
+}
+
+export default Error;
