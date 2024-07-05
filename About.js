c1d432ae61a467c5d1475bb4084e22a6b4ccb305
surbhikri18 <sk3519715@gmail.com>
2024-07-06 03:56:23 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,14 @@
+const About = () => {
+    return( 
+        <div>
+            <h1>About Us Page</h1>
+            <p>
+                {" "}
+                This is a Food Villa App, which is a
+                Food Ordering App
+            </p>
+        </div>  
+    );
+}
+
+export default About;
